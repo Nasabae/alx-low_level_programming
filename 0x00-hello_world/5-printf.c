2c2a04c08f1553dfaf4e92282e8ea66d75036e3f
Nasabae <ssakoto11@gmail.com>
2023-04-20 16:42:44 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Printf function should be used for this program
+ * Return: 0 (Win)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
