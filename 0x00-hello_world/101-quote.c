5145301dedd8c4b0b3ae1562d7e0644fffa9b03d
Nasabae <ssakoto11@gmail.com>
2023-04-20 18:31:36 -0700

    task8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Print a program to a standard error
+ * Return: 1 (Win)
+ */
+int main(void)
+{
+write(2,
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
