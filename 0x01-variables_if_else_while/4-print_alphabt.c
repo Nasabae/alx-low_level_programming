f5eb93d2244316fba45639c19fec3add45d039ca
Nasabae <ssakoto11@gmail.com>
2023-04-22 10:24:32 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet without q and e
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char la;
+for (la = 'a'; la <= 'z'; la++)
+{
+if (la != 'e' && la != 'q')
+putchar(la);
+}
+return (0);
+}
