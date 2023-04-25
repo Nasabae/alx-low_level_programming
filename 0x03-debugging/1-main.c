77d58aa55ce1c4cd0ae5b20c873ab8518df7f01f
Nasabae <ssakoto11@gmail.com>
2023-04-25 16:56:35 -0700

    task 1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+int main(void)
+{
+int s;
+printf("Infinite loop incoming :(\n");
+s = 0;
+/*while (s < 10)*/
+/*{*/
+/*   putchar(s);*/
+/*}*/
+printf("Infinite loop avoided! \\o/\n");
+return (0);
+}
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - determine if a number is positive, negative or zero
+ * 0 : number to be printed
+ * Return: 0 on success
+ */
+void positive_or_negative(int i)
+{
+else if ('i' < 0)
+{
+printf("%d is %s\n", 'i', "negative");
+}
+if ('i' > 0)
+{
+printf("%d is %s\n", 'i', "positive");
+}
+else
+{
+printf("%d is %s\n", 'i', "zero");
+}
+return (0);
+}
