ca7f3031607a3e51254f81612410024283ea8ae0
Nasabae <ssakoto11@gmail.com>
2023-05-04 17:40:29 -0700

    104-print_buffer.c

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_buffer - prints buffer
+ * @b: buffer
+ * @size: size
+ * Return: void
+ */
+void print_buffer(char *b, int size)
+{
+int a, d, c;
+a = 0;
+if (size <= 0)
+{
+printf("\n");
+return;
+}
+while (a < size)
+{
+d = size - a < 10 ? size - a : 10;
+printf("%08x: ", a);
+for (c = 0; c < 10; c++)
+{
+if (c < d)
+printf("%02x", *(b + a + c));
+else
+printf("  ");
+if (c % 2)
+{
+printf(" ");
+}
+}
+for (c = 0; c < d; c++)
+{
+int e = *(b + a + c);
+if (e < 32 || e > 132)
+{
+e = '.';
+}
+printf("%c", e);
+}
+printf("\n");
+a += 10;
+}
+}
