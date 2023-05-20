68c623e928a108a36bcd766499c45fbd65626f35
Nasabae <ssakoto11@gmail.com>
2023-05-19 17:54:41 -0700

    2-print_strings.c

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - prints strings
+ * @separator: separator between strings
+ * @n: number of arguments
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+unsigned int z;
+char *str;
+va_list list;
+va_start(list, n);
+for (z = 0; z < n; z++)
+{
+str = va_arg(list, char *);
+if (!str)
+str = "(nil)";
+if (!separator)
+printf("%s", str);
+else if (separator && z == 0)
+printf("%s", str);
+else
+printf("%s%s", separator, str);
+}
+printf("\n");
+va_end(list);
+}
