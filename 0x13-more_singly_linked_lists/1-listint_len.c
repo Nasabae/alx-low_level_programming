1a927f35f077e6de6081158057447f541744d93c
Nasabae <ssakoto11@gmail.com>
2023-05-31 08:29:47 -0700

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+  * listint_len - Counts the elements in a linked list
+  * @h: The head of the linked list
+  * Return: The number of elements in a linked list
+  */
+size_t listint_len(const listint_t *h)
+{
+size_t total = 0;
+while (h)
+{
+total++;
+h = h->next;
+}
+return (total);
+}
