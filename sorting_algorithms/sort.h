c3e53d3e32e166846479b3339f2b58a8f1291473
Nasabae <ssakoto11@gmail.com>
2023-08-25 08:31:47 -0700

    sort.h

--- sorting_algorithms/sort.h
+++ sorting_algorithms/sort.h
@@ -22,21 +22,21 @@ struct listint_s *prev;
 struct listint_s *next;
 } listint_t;
 
-void print_array(const int *, size_t);
-void print_list(const listint_t *);
-void bubble_sort(int *array, size_t size);
-void insertion_sort_list(listint_t **list);
-void selection_sort(int *array, size_t size);
-void quick_sort(int *array, size_t size);
-void quick_sort_rec(int *array, int lower, int higher, size_t size);
-int lomuto_partition(int *array, int lower, int higher, size_t size);
-void shell_sort(int *array, size_t size);
-void counting_sort(int *array, size_t size);
-void radix_sort(int *array, size_t size);
-void bitonic_sort(int *array, size_t size);
-void cocktail_sort_list(listint_t **list);
-void swap_nodes(listint_t **list, listint_t *node);
+void ssa_print_array(const int *, size_t);
+void ssa_print_list(const listint_t *);
+void ssa_bubble_sort(int *array, size_t size);
+void ssa_insertion_sort_list(listint_t **list);
+void ssa_selection_sort(int *array, size_t size);
+void ssa_quick_sort(int *array, size_t size);
+void ssa_quick_sort_rec(int *array, int lower, int higher, size_t size);
+int ssa_lomuto_partition(int *array, int lower, int higher, size_t size);
+void ssa_shell_sort(int *array, size_t size);
+void ssa_counting_sort(int *array, size_t size);
+void ssa_radix_sort(int *array, size_t size);
+void ssa_bitonic_sort(int *array, size_t size);
+void ssa_cocktail_sort_list(listint_t **list);
+void ssa_swap_nodes(listint_t **list, listint_t *node);
 listint_t *get_dlistint_lelem(listint_t *h);
-int getCantRep(int num);
+int ssa_getCantRep(int num);
 
 #endif
